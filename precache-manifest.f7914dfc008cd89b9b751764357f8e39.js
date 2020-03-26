3122c5ec1591b0270c4c01991a3a9f14e09d35da
Varun Peesapati <peesapativarun99@gmail.com>
2020-03-26 07:40:52 -0700

    Updates

---
+++ precache-manifest.f7914dfc008cd89b9b751764357f8e39.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be74dc0eccbffd558299b1e5d3553f0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9449f70885f392814ca1",
+    "url": "/static/css/main.ea7e0905.chunk.css"
+  },
+  {
+    "revision": "e9685764e1795c0a2ef0",
+    "url": "/static/js/2.59756631.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.59756631.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9449f70885f392814ca1",
+    "url": "/static/js/main.4a7743c4.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "3d1ca5d42f0b12ad86198a5386cea8fe",
+    "url": "/static/media/My Resume.3d1ca5d4.pdf"
+  },
+  {
+    "revision": "072e913c3cc9a8fab95f79a423302f5e",
+    "url": "/static/media/portfolio-img.072e913c.jpg"
+  }
+]);
