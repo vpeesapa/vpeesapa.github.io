fbe29641cc21e94e5039e463a7451f488aff9351
Varun Peesapati <peesapativarun99@gmail.com>
2020-03-26 06:53:13 -0700

    Updates

---
+++ precache-manifest.3eba74c67d28e3c7dd89b504d85151c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdad12972c5ba087a8bdbec2e744a318",
+    "url": "/index.html"
+  },
+  {
+    "revision": "981f89fa6bdd646f79bb",
+    "url": "/static/css/main.931d697d.chunk.css"
+  },
+  {
+    "revision": "05b335fca16838096880",
+    "url": "/static/js/2.dd3d76f6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.dd3d76f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "981f89fa6bdd646f79bb",
+    "url": "/static/js/main.4259bfdc.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "072e913c3cc9a8fab95f79a423302f5e",
+    "url": "/static/media/portfolio-img.072e913c.jpg"
+  }
+]);
