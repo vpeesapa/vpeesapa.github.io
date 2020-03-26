f4f4e88d84d0e09ca5cfd1d358d40d8a29f576f2
Varun Peesapati <peesapativarun99@gmail.com>
2020-03-26 06:17:35 -0700

    Added an about page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import './App.css';
+import portfolioImg from './img/portfolio-img.jpg';
 
 class App extends React.Component {
 	render() {
 		return (
 			<div>
-				<p>Website is being developed with better UI implementations!</p>
+				<div className="header">
+					<h1>My Portfolio</h1>
+				</div>
+				<div className="navbar">
+					<a href="#">Home</a>
+					<a href="#">Experience</a>
+					<a href="#">Contact Information</a>
+				</div>
+				<div className="about-container">
+					<div className="image-container">
+						<img alt="my pic" src={portfolioImg}/>
+					</div>
+					<div className="intro-container">
+						<p>Hi there! My name is Varun Peesapati! Welcome to my website!</p>
+					</div>
+				</div>
 			</div>
 		);
 	}
